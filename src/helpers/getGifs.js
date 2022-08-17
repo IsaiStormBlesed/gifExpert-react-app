4fdf4fbd2b17001a1425039f8febb9034e55a437
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-08-17 23:12:34 +0000

    env

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,7 @@
 export const getGifs = async (category) => {
-  const API_URL = `https://api.giphy.com/v1/gifs/search?api_key=CYe4G7wcFaBgUX90FL13i5tUtzXJ3Qzg&q=${category}&limit=5`
+  const API_URL = `https://api.giphy.com/v1/gifs/search?api_key=${
+    import.meta.env.VITE_GIPHY_API_KEY
+  }&q=${category}&limit=5`
   const response = await fetch(API_URL)
   const { data } = await response.json()
 
